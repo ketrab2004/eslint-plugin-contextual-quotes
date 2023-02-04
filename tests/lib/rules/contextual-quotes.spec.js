baa34d604139e361070b84df118c1dff7526f4de
Bartek O <bartek@fos.nl>
2023-02-05 00:21:05 +0100

    Replaced every 4 spaces with a tab to reduce file sizes

--- tests/lib/rules/contextual-quotes.spec.js
+++ tests/lib/rules/contextual-quotes.spec.js
@@ -3,35 +3,35 @@
 const { RuleTester } = require("eslint")
 const contextualQuotes = require("../../../lib/rules/contextual-quotes.js")
 const ruleTester = new RuleTester({
-    parserOptions: {
-        ecmaVersion: 2015
-    }
+	parserOptions: {
+		ecmaVersion: 2015
+	}
 })
 
 ruleTester.run("contextual-quotes", contextualQuotes, {
-    valid: [
-        { code: "const a = 'a'" },
-        { code: "const a = \"word\""},
-        { code: "const a = `something ${a}`" },
+	valid: [
+		{ code: "const a = 'a'" },
+		{ code: "const a = \"word\""},
+		{ code: "const a = `something ${a}`" },
 
-        {
-            code: "const a = '😎'",
-            options: [{
-                emoijAsSingleCharacter: "always"
-            }]
-        }
-    ],
-    invalid: [
-        { code: "const a = \"a\"", errors: [{ messageId: "character" }] },
-        { code: "const a = `a`", errors: [{ messageId: "character" }] },
+		{
+			code: "const a = '😎'",
+			options: [{
+				emoijAsSingleCharacter: "always"
+			}]
+		}
+	],
+	invalid: [
+		{ code: "const a = \"a\"", errors: [{ messageId: "character" }] },
+		{ code: "const a = `a`", errors: [{ messageId: "character" }] },
 
-        { code: "const a = \"\"", errors: [{ messageId: "empty" }] },
-        { code: "const a = ``", errors: [{ messageId: "empty" }] },
+		{ code: "const a = \"\"", errors: [{ messageId: "empty" }] },
+		{ code: "const a = ``", errors: [{ messageId: "empty" }] },
 
-        { code: "const a = 'aaa'", errors: [{ messageId: "string" }] },
-        { code: "const a = `aaa`", errors: [{ messageId: "string" }] },
-        { code: "const a = '😎'", errors: [{ messageId: "string" }] }
+		{ code: "const a = 'aaa'", errors: [{ messageId: "string" }] },
+		{ code: "const a = `aaa`", errors: [{ messageId: "string" }] },
+		{ code: "const a = '😎'", errors: [{ messageId: "string" }] }
 
-        // { code: "const  = ", errors: [{ messageId: "" }] }
-    ]
+		// { code: "const  = ", errors: [{ messageId: "" }] }
+	]
 });
